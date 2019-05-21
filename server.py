af0a98525aab49e64def2540c0521d241894eb09
liuhx9988 <liuhx1122@gmail.com>
2019-05-21 00:26:03 -0700

    server update

--- server.py
+++ server.py
@@ -1,77 +1,98 @@
-import turtle
-import time
-import random
 from socket import *
+from _thread import *
+import random
 import sys
-
-# import queue
 from collections import deque
 
-
-serverName = '72.233.138.35'
-serverPort = 43500
-clientSocket = socket(AF_INET, SOCK_STREAM)  # TCP socket
-clientSocket.connect((serverName, serverPort))
 # constant
-
 WIDTH = 1000
 HEIGHT = 1000
+REAL_WIDTH = WIDTH/20
+REAL_HEIGHT = HEIGHT/20
 DELAY = 0.1  # speed of the game
 
-
-# main screen
-game = turtle.Screen()
-game.tracer(0)
-game.bgcolor("black")
-game.title("Sanke Game")
-game.setup(width=WIDTH, height=HEIGHT)
-
-
-def send_data(dir):
-    data = str(dir)
-    clientSocket.send(data.encode('utf-8'))
+# setup socket to wait for connections
+serverPort = 43500
+serverSocket = socket(AF_INET, SOCK_STREAM)  # TCP (reliable)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # make port reusable
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
 
 
+def receive_message():
+    message = serverSocket.recv(1024).decode('utf-8')
+    # receive message from server, print it, close connection
+    print('From client', message)
+
+
+
+def broadcast(message, connection):
+    for client in clients:
+        if client[0] != connection:
+            try:
+                client[0].send(message).encode('utf-8')
+            except:
+                client[0].close()
+
+                # if the link is broken, we remove the client
+                remove(clients)
+#remove client if there isn't any more
+def remove(connection):
+    if connection in clients:
+        clients.remove(connection)
+
+def on_new_client(connection,addr):
+    while True:
+        msg = connection.recv(1024).decode('utf-8')
+        broadcast(msg, connection)
+
+
+
+clients = []
+
+#accept up to two connections from clients, which
+# must connect before we can move on
+for i in range(0, MAX_CLIENTS):
+    connectionSocket, addr = serverSocket.accept()
+    clients.append((connectionSocket,addr))
+    print (addr[0] +':'+str(addr[1]) + " connected" )
+    start_new_thread(on_new_client,(connectionSocket,addr))
+
+
+# game
 class Player:
     # player head
     def __init__(self, color):
-        self.head = turtle.Turtle()
-        self.direction = "stop"
         self.length = 20
-        self.head.speed(0)
-        self.head.shape("square")
-        self.head.color(color)
-        self.head.penup()
+        self.direction = "stop"
+        self.color(color)
         self.alive = True
         # self.head.goto(0,0)   # start cor
-        x = random.randint(1-WIDTH/2, WIDTH/2-1)
-        x = x - x % 20
-        y = random.randint(1-HEIGHT/2, HEIGHT/2-1)
-        y = y - y % 20
-        self.head.goto(x, y)
+        self.x = random.randint(1-REAL_WIDTH/2, REAL_WIDTH/2-1)
+        self.y = random.randint(1-REAL_HEIGHT/2, REAL_HEIGHT/2-1)
         self.body = deque()
     # player movement
 
     def move(self):
         if self.direction == "up":
-            self.head.sety(self.head.ycor() + 20)
-            if self.head.ycor()>499:
-                self.head.sety(-self.head.ycor())
+            self.y = self.y + 1
+            if self.y > REAL_HEIGHT/2-1:
+                self.y = -self.y
         if self.direction == "down":
-            self.head.sety(self.head.ycor() - 20)
-            if self.head.ycor() < -499:
-                self.head.sety(-self.head.ycor())
+            self.y = self.y - 1
+            if self.y < 1 - REAL_HEIGHT/2:
+                self.y = - self.y
         if self.direction == "left":
-            self.head.setx(self.head.xcor() - 20)
-            if self.head.xcor() < -499:
-                self.head.setx(-self.head.xcor())
+            self.x = self.x - 1
+            if self.x < 1 - REAL_WIDTH/2:
+                self.x = - self.x
         if self.direction == "right":
-            self.head.setx(self.head.xcor() + 20)
-            if self.head.xcor() > 499:
-                self.head.setx(-self.head.xcor())
+            self.x = self.x + 1
+            if self.x > REAL_WIDTH/2-1:
+                self.x = -self.x
 
     def eat(self):
-        self.length +=1
+        self.length += 1
 
     # player movement control not allow go back
     def up(self):
@@ -91,37 +112,25 @@ class Player:
             self.direction = "right"
 
 
-class Body: # base segment of snake
+class Body:  # base segment of snake
     def __init__(self, p):
         self.body = p
-        self.seg = turtle.Turtle()
-        self.seg.speed(0)
-        self.seg.shape("square")
-        self.seg.color("white")
-        self.seg.penup()
-        self.seg.goto(p.head.xcor(), p.head.ycor())
+        self.x
+        self.y
 
     def update(self):
-        self.seg.goto(self.body.head.xcor(), self.body.head.ycor())
+        self.x = self.body.x
+        self.y = self.body.y
 
 
 class Food:
     def __init__(self):
-        self.food = turtle.Turtle()
-        self.direction = "stop"
-        self.food.speed(0)
-        self.food.shape("circle")
-        self.food.color("green")
-        self.food.penup()
-        self.food.goto(0, 200)   # start cor
+        self.x = 0
+        self.y = 200  # start cor
 
     def move(self):
-        x = random.randint(-499, 499)
-        x= x- x%20
-        y = random.randint(-499, 499)
-        y = y - y % 20
-        self.food.goto(x, y)
-
+        self.x = random.randint(1-REAL_WIDTH/2, REAL_WIDTH/2-1)
+        self.y = random.randint(1-REAL_HEIGHT/2, REAL_HEIGHT/2-1)
 
 player1 = Player("red")
 player2 = Player("blue")
@@ -130,19 +139,10 @@ playercount = 2
 players= [player1, player2]  # ,player3]
 food = Food()
 
-game.listen()
-game.onkey(player1.up, "w")
-game.onkey(player1.down, "s")
-game.onkey(player1.left, "a")
-game.onkey(player1.right, "d")
 
-game.onkey(player2.up, "Up")
-game.onkey(player2.down, "Down")
-game.onkey(player2.left, "Left")
-game.onkey(player2.right, "Right")
+
+# main loop
 while True:
-    game.update()
-
     for p in players:
         if p.head.xcor() == food.food.xcor()and p.head.ycor() == food.food.ycor():
             p.eat()
@@ -180,6 +180,3 @@ while True:
     # text = turtle.write("playercount : " + str(playercount), move=False, align="left", font=("Arial", 8, "normal"), color = "white")
 
     time.sleep(DELAY)
-
-
-game.mainloop()
